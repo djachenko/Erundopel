b114fef06bdad85d088891ae7074fbb2c527ffe8
Alexey Ershov <alaershov@gmail.com>
2014-07-28 14:03:57 +0700

    Create basic Xcode project
    Created default basic Xcode project and directories for code and docs.

---
+++ src/Erundopel/ErundopelTests/ErundopelTests.m
@@ -0,0 +1,34 @@
+//
+//  ErundopelTests.m
+//  ErundopelTests
+//
+//  Created by Admin on 28/07/14.
+//  Copyright (c) 2014 Noveo Summer Internship. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ErundopelTests : XCTestCase
+
+@end
+
+@implementation ErundopelTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
