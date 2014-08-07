79c038dcd9057b3854b48d75b896e369e2e05c43
Alexey Ershov <alaershov@gmail.com>
2014-08-07 20:21:31 +0700

    Started adding Parse synchronisation

---
+++ src/Erundopel/Erundopel/ParseManager.m
@@ -0,0 +1,13 @@
+//
+//  ParseManager.m
+//  Erundopel
+//
+//  Created by Admin on 07/08/14.
+//  Copyright (c) 2014 Noveo Summer Internship. All rights reserved.
+//
+
+#import "ParseManager.h"
+
+@implementation ParseManager
+
+@end
---
+++ src/Erundopel/Erundopel/ParseObject.h
@@ -0,0 +1,13 @@
+//
+//  ParseObject.h
+//  Erundopel
+//
+//  Created by Admin on 07/08/14.
+//  Copyright (c) 2014 Noveo Summer Internship. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ParseObject <NSObject>
+
+@end
